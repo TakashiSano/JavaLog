73a5eda75252331ddec4a2e165da7c93085dca18
307156 <t-sano@t-gaia.co.jp>
2016-12-08 15:09:12 +0900

    @Override追加

--- app/src/main/java/jp/teachacademy/takashi/sano/javalog/Human.java
+++ app/src/main/java/jp/teachacademy/takashi/sano/javalog/Human.java
@@ -10,13 +10,14 @@ class Human extends Animal implements Thinkable {
 
     static String to_jp = "人";
 
-    
+
     public Human(String name, int age, String hobby){
         this.name = name;
         this.age = age;
         this.hobby = hobby;
     }
 
+    @Override
     public void say(){
         Log.d("javatest","私の名前は" + this.name + "です。年は" + this.age +"歳です。");
     }
